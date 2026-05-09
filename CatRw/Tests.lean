793ffdf6d61819bf5a7cf4e6d68a9a6e33a5d8dc
Brian-Nugent <bnugent@uw.edu>
2026-05-09 14:16:33 -0700

    merge

--- CatRw/Tests.lean
+++ CatRw/Tests.lean
@@ -42,6 +42,14 @@ example (ha : a ≅ a') (hb : b ≅ b') : (a ⨯ b) ⨯ c ≅ (b' ⨯ c) ⨯ a' 
 
 /- A more basic example of what we want cat-rw to do -/
 example (ha : a ≅ a') : F.obj a ≅ F.obj a' := by
+  exact F.mapIso ha
+
+/-
+example (ha : a ≅ a') : F.obj a ≅ F.obj a' := by
+  cat_rw [ha]
+-/
+
+example (ha : a ≅ a') : F.obj a ≅ F.obj a' := by
   cat_rw [ha]
 
 example (h : F ≅ G) : F.obj a ≅ G.obj a := by
@@ -52,6 +60,13 @@ example (h : F ≅ G) : F.obj a ≅ G.obj a := by
 -/
 
 example (h : F ≅ G) : F.obj a ≅ G.obj a := by
+  cat_rw [h]
+
+example (ha : a ≅ a') (h : F ≅ G) : F.obj a ≅ G.obj a' := by
+  cat_rw [h, ha]
+
+def iso₁ (ha : a ≅ a') (h : F ≅ G) : F.obj a ≅ G.obj a' := by
+  cat_rw [h, ha]
 
 def iso₂ (ha : a ≅ a') (h : F ≅ G) : F.obj a ≅ G.obj a' := by
   cat_rw [ha, h]

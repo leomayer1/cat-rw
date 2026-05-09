f69d5b8ec3c7495b99e2efa5bb81973d739e0d8f
Leopold Mayer <leomayer@uw.edu>
2026-05-09 15:36:46 -0700

    more examples

--- CatRw/Tests.lean
+++ CatRw/Tests.lean
@@ -112,6 +112,7 @@ lemma pres_eq (φ : F ≅ G) :
 example (φ : F ≅ G) (M : (C ⥤ D) ⥤ (C ⥤ D)) :
     (M.obj G).IsEquivalence := by
   show_term cat_rw [← φ]
+  sorry
 
 noncomputable
 example (h : a ≅ a') : a ⨯ b ≅ a' ⨯ b := by
@@ -147,7 +148,21 @@ example (ha : a ≅ a') : IsZero (a ⨿ b) := by
    After apply cat_rw [ha], the new goal state should be a' ⨯ b ≅ a' ⨯ b
 -/
 example (ha : a ≅ a') : a ⨯ b ≅ a' ⨯ b := by
+  --cat_rw [ha]
+  sorry
+
+#check prod.functor
+
+theorem prod_eq_obj : (a ⨯ b) = (prod.functor.obj a).obj b := rfl
+
+noncomputable
+example (ha : b ≅ b') : a ⨯ b ≅ a ⨯ b' := by
+  rw [prod_eq_obj, prod_eq_obj]
   cat_rw [ha]
+
+example (ha : a ≅ a') : a ⨯ b ≅ a' ⨯ b := by
+  rw [prod_eq_obj, prod_eq_obj]
+  --cat_rw [ha]
   sorry
 
 example (h₁ : F ≅ G) (h₂ : F.obj a ≅ x) (h₃ : F.obj b ≅ x) (h₄ : H ≅ K) :
